c3cc28e8279761b4c4a344a92de2a98a98b074b0
ohcibi <ohcibi@dwgadf.de>
2012-05-12 21:18:35 +0200

    Add test for gutter game

---
+++ src/de/hhu/propra2012/beispiele/bowling/BowlingGameTest.java
@@ -0,0 +1,15 @@
+package de.hhu.propra2012.beispiele.bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void testGutterGame() {
+		Game g = new Game();
+		for (int i = 0; i < 20; i++) g.roll(0);
+		assertEquals(0, g.score());
+	}
+}
---
+++ src/de/hhu/propra2012/beispiele/bowling/Game.java
@@ -0,0 +1,12 @@
+package de.hhu.propra2012.beispiele.bowling;
+
+public class Game {
+	
+	public void roll(int pins) {
+		
+	}
+	
+	public int score() {
+		return 0;
+	}
+}
