56c5a65d1eb03ca45d946f1c6c74afe05fc027f4
Dennis Esken <dennis.esken@hhu.de>
2012-05-12 17:13:14 +0200

    Changed to JUnit4

--- src/junit/TaschenrechnerTest.java
+++ src/junit/TaschenrechnerTest.java
@@ -1,72 +1,42 @@
 package junit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Testklasse für Methoden der Klasse Taschenrechner.
  */
-public class TaschenrechnerTest extends TestCase {
-	
+public class TaschenrechnerTest {
+
 	/** Der Taschenrechner, der getestet wird. */
 	private Taschenrechner taschenrechner;
-	
-	/**
-	 * Erzeugt ein Objekt dieser Klasse mit angegebenem Namen.
-	 * @param name Name des Tests.
-	 */
-	public TaschenrechnerTest(String name) {
-		
-		super(name);
-	}
-	
-	/**
-	 * Liefert die Suite der Testmethoden
-	 * @return Testmethoden dieser Klasse
-	 */
-	public static Test suite() {
-	    
-	    return new TestSuite(TaschenrechnerTest.class);
-	}
-	
+
 	/**
 	 * Testdaten aufbauen
 	 */
-	protected void setUp() {
-		
-		taschenrechner = new Taschenrechner();
+	@Before
+	public void setUp() {
+		this.taschenrechner = new Taschenrechner();
 	}
-	
+
 	/**
 	 * Testet die Methode addiere der Klasse Taschenrechner.
 	 */
+	@Test
 	public void testAddiere() {
-		
-		assertEquals(3 + 4, taschenrechner.addiere(3, 3));
+		assertEquals(3 + 3, taschenrechner.addiere(3, 3));
 		assertEquals(-1 + -1, taschenrechner.addiere(-1, -1));
 	}
-	
+
 	/**
 	 * Testet die Methode multipliziere der Klasse Taschenrechner.
 	 */
+	@Test
 	public void testMultipliziere() {
-		
 		assertEquals(3 * 4, taschenrechner.multipliziere(3, 4));
 		assertEquals(-1 * -1, taschenrechner.multipliziere(-1, -1));
 	}
-	
-	/**
-	 * Startet den Test
-	 * @param args Wird nicht verwendet
-	 */
-	public static void main(String[] args) {
-	    
-	    /* textueller TestRunner */
-	    junit.textui.TestRunner.run(suite());
-	    
-	    /* grafischer TestRunner */
-//	    junit.swingui.TestRunner.run(TaschenrechnerTest.class);
-	  }
 
 }
