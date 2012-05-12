ba73873547323185a347f3bccd094bddfa5220bd
ohcibi <ohcibi@dwgadf.de>
2012-05-12 21:24:01 +0200

    Refactor BowlingGameTest

--- src/de/hhu/propra2012/beispiele/bowling/BowlingGameTest.java
+++ src/de/hhu/propra2012/beispiele/bowling/BowlingGameTest.java
@@ -2,21 +2,30 @@ package de.hhu.propra2012.beispiele.bowling;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
+	private Game g;
+	
+	@Before
+	public void setUp() {
+		g = new Game();
+	}
 
 	@Test
 	public void testGutterGame() {
-		Game g = new Game();
-		for (int i = 0; i < 20; i++) g.roll(0);
+		rollN(20, 0);
 		assertEquals(0, g.score());
 	}
 	
 	@Test
 	public void testAllOnes() {
-		Game g = new Game();
-		for (int i = 0; i < 20; i++) g.roll(1);
+		rollN(20, 1);
 		assertEquals(20, g.score());
+	}
+	
+	private void rollN(int n, int pins) {
+		for (int i = 0; i < n; i++) g.roll(pins);
 	}
 }
