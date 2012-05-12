c1def839039559da6e838025eaa8c2f5fbc20847
ohcibi <ohcibi@dwgadf.de>
2012-05-12 21:20:01 +0200

    Add test for game with all ones

--- src/de/hhu/propra2012/beispiele/bowling/BowlingGameTest.java
+++ src/de/hhu/propra2012/beispiele/bowling/BowlingGameTest.java
@@ -12,4 +12,11 @@ public class BowlingGameTest {
 		for (int i = 0; i < 20; i++) g.roll(0);
 		assertEquals(0, g.score());
 	}
+	
+	@Test
+	public void testAllOnes() {
+		Game g = new Game();
+		for (int i = 0; i < 20; i++) g.roll(1);
+		assertEquals(20, g.score());
+	}
 }
--- src/de/hhu/propra2012/beispiele/bowling/Game.java
+++ src/de/hhu/propra2012/beispiele/bowling/Game.java
@@ -1,12 +1,13 @@
 package de.hhu.propra2012.beispiele.bowling;
 
 public class Game {
+	private int score = 0;
 	
 	public void roll(int pins) {
-		
+		score += pins;
 	}
 	
 	public int score() {
-		return 0;
+		return score;
 	}
 }
