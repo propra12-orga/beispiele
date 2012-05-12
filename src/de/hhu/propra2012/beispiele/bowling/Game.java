d796ae04323ef915c78ce814874e3554a16b88fc
ohcibi <ohcibi@dwgadf.de>
2012-05-12 21:28:48 +0200

    Refactor Game to calculate score in Game.score and count pins in Game.roll and not the other way around

--- src/de/hhu/propra2012/beispiele/bowling/Game.java
+++ src/de/hhu/propra2012/beispiele/bowling/Game.java
@@ -1,13 +1,23 @@
 package de.hhu.propra2012.beispiele.bowling;
 
 public class Game {
-	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 	
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 	
 	public int score() {
+		int score = 0, frameIndex = 0;
+		for (int frame = 0; frame < rolls.length/2; frame++) {
+			score += scoreInFrame(frameIndex);
+			frameIndex += 2;
+		}
 		return score;
+	}
+
+	private int scoreInFrame(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1];
 	}
 }
